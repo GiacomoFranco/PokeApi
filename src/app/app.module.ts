9f90065f8b9343adf7a366b78f3be423a7dbd67d
Passiflora <passiflora.freelance@gmail.com>
2022-05-26 23:52:24 -0500

    Search Pokemon UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchPokemonComponent } from './search-pokemon/search-pokemon.component';
+import { PokemonGalleryComponent } from './pokemon-gallery/pokemon-gallery.component';
+import { PokemonStatsComponent } from './pokemon-stats/pokemon-stats.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPokemonComponent,
+    PokemonGalleryComponent,
+    PokemonStatsComponent
   ],
   imports: [
     BrowserModule,
